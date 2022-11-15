c1a0dea8a01d583c6cb1c3766e399fbee6e8d285
maximrockon <maximpopov200343@gmail.com>
2022-11-15 11:45:03 -0800

    Modify main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 
 int main(int argc, char* argv[]) {
 	if (argc > 2) {
-		std::cerr << "Invalid number of args";
+		std::cerr << "Invalid number of args\n";
 		return -1;
 	} else {
 		RequestHandler handler;
 		if (argc == 2) {
 			std::ifstream in(argv[1]);
 			if (!in.is_open()) {
-				std::cerr << "Invalid filename";
+				std::cerr << "Invalid filename\n";
 				return -1;
 			}
 			handler.execute(&in, new SelfBalancingBinaryTree<int>());
